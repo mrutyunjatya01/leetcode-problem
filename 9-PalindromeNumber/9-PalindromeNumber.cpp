44e376b8215e8923e4064b74c765f6d5c45f5eaa
mrutyunjatya01 <mrutyunjaya407@gmail.com>
2026-07-13 16:20:54 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -0,0 +1,15 @@
+// Last updated: 13/07/2026, 16:20:54
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0)) return false;
+
+        int num=0;
+        while(x>num){
+            num=(num*10)+ (x%10);
+            x/=10;
+        }
+
+        return (x==num || x==num/10);
+    }
+};
