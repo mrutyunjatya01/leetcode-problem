6b9cfa605c986de41cfd4cf6bc49fd350a0605b1
mrutyunjatya01 <mrutyunjaya407@gmail.com>
2026-07-13 16:16:33 +0530

    Create 264-UglyNumberIi

---
+++ 264-UglyNumberIi/264-UglyNumberIi.cpp
@@ -0,0 +1,26 @@
+// Last updated: 13/07/2026, 16:16:33
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        vector<int>dp( n );
+        dp[0]=1;
+
+        int i2=0;
+        int i3=0;
+        int i5=0;
+
+        for(int i=1; i<n; i++){
+        int next2=dp[i2]*2;
+        int next3=dp[i3]*3;
+        int next5=dp[i5]*5;
+
+        int nextUgly=min({next2, next3, next5});
+        dp[i]=nextUgly;
+
+        if(nextUgly==next2) i2++;
+        if(nextUgly==next3) i3++;
+        if(nextUgly==next5) i5++;
+    }
+    return dp[n-1];
+    }
+};
